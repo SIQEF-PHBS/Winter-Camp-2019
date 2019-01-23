bc741713b5706235b1ff17310cac5120188633d9
beezbbees <13447786+beezbbees@users.noreply.github.com>
2019-01-24 01:37:43 +0800

    Add files via upload

---
+++ Xianhua Peng/sinkhorn_knopp.py
@@ -0,0 +1,32 @@
+"""
+Implementation of the Sinkhorn-Knopp algorithm for optimal transport
+"""
+
+import numpy as np
+
+def compute_optimal_transport(M, r, c, lam, epsilon=1e-8):
+    """
+    Computes the optimal transport matrix and Slinkhorn distance using the
+    Sinkhorn-Knopp algorithm
+
+    Inputs:
+        - M : cost matrix (n x m)
+        - r : vector of marginals (n, )
+        - c : vector of marginals (m, )
+        - lam : strength of the entropic regularization
+        - epsilon : convergence parameter
+
+    Output:
+        - P : optimal transport matrix (n x m)
+        - dist : Sinkhorn distance
+    """
+    n, m = M.shape
+    P = np.exp(- lam * M)
+    P /= P.sum()
+    u = np.zeros(n)
+    # normalize this matrix
+    while np.max(np.abs(u - P.sum(1))) > epsilon:
+        u = P.sum(1)
+        P *= (r / u).reshape((-1, 1))
+        P *= (c / P.sum(0)).reshape((1, -1))
+    return P, np.sum(P * M)
